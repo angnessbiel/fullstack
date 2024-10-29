3aee283d0829424b7b253b3f4f212c944b0c6f95
GABRIEL MENDES ANGNES <gabriel_angnes@estudante.sesisenai.org.br>
2024-10-29 12:25:42 -0300

    e

--- server/index.js
+++ server/index.js
@@ -1,42 +1,13 @@
 import Express from "express"
-import { criarTabelas, User } from "./db.js"
-import bcryptjs from "bcryptjs"
+// import { criarTabelas } from "./db.js"
+import cors from "cors"
+import {rotas} from './roteamento/rotas_autenticadas.js'
 
 const app = Express()
 app.use(Express.json())
+app.use(cors())
 //criarTabelas()
 
-app.post('/registro', async (req, res) => {
-    const {nome, sobrenome, email, senha, dataNascimento} = req.body
-    if(!nome || !sobrenome || !email || !senha || !dataNascimento){
-        res.send('Todos os campos devem ser preenchidos')
-    }
-    const userExist = await User.findOne({email :email})    
-    if (userExist){
-        res.send('usuario ja existe')
-        return
-    }
-    const senhaCriptografada = bcryptjs.hashSync(senha, 10)
-    
-    const teste = await User.create({nome, sobrenome, email, senha: senhaCriptografada, dataNascimento})
-    res.send('ok usuario criado')
-
-})
-
-app.post('/login',async (req, res) => {
-const {email, senha} = req.body
-if (!email || !senha) {
-}
-const userExist = await User.findOne({email :email})    
-if (!userExist){
-    res.send('usuario nao existe')
-    return
-}
-const senhaValida = bcryptjs.compareSync(senha, userExiste.senha)
-if (!senhaValida){
-    res.send('senha invalida')
-    return
-}
-})
+app.use('/autenticacao', rotas)
 
 app.listen(8000) 
